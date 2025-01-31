3deec9b38b3e500dadd6136d7a35e2e372b0336c
Eray Ates <eray.ates@worldline.com>
2025-01-31 11:53:42 +0100

    feat: add time parse
    Signed-off-by: Eray Ates <eray.ates@worldline.com>

--- time.go
+++ time.go
@@ -43,6 +43,19 @@ func (t *Time) UnmarshalJSON(data []byte) error {
 	return fmt.Errorf("invalid time format: %s", s)
 }
 
+func (t *Time) Parse(s string) error {
+	for _, format := range timeFormats {
+		tt, err := time.Parse(format, s)
+		if err == nil {
+			t.Time = tt
+
+			return nil
+		}
+	}
+
+	return fmt.Errorf("invalid time format: %s", s)
+}
+
 // String returns the time in RFC3339 format.
 func (t Time) String() string {
 	return t.Time.Format(time.RFC3339)
