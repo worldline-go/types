cd58934d7735f1130c2e7f954c338a35801892b7
Eray Ates <eray.ates@worldline.com>
2025-02-06 10:50:46 +0100

    feat: null time
    Signed-off-by: Eray Ates <eray.ates@worldline.com>

--- time.go
+++ time.go
@@ -17,10 +17,70 @@ var timeFormats = []string{
 
 type Time struct {
 	time.Time
-	Valid bool
 }
 
 func (t Time) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + t.Time.Format(time.RFC3339) + `"`), nil
+}
+
+func (t *Time) UnmarshalJSON(data []byte) error {
+	s := strings.Trim(string(data), `"`)
+
+	for _, format := range timeFormats {
+		tt, err := time.Parse(format, s)
+		if err == nil {
+			t.Time = tt
+
+			return nil
+		}
+	}
+
+	return fmt.Errorf("invalid time format: %s", s)
+}
+
+func (t *Time) Parse(s string) error {
+	for _, format := range timeFormats {
+		tt, err := time.Parse(format, s)
+		if err == nil {
+			t.Time = tt
+
+			return nil
+		}
+	}
+
+	return fmt.Errorf("invalid time format: %s", s)
+}
+
+// String returns the time in RFC3339 format.
+func (t Time) String() string {
+	return t.Time.Format(time.RFC3339)
+}
+
+// Scan implements the [Scanner] interface.
+func (t *Time) Scan(value any) error {
+	v := sql.NullTime{}
+	if err := v.Scan(value); err != nil {
+		return err
+	}
+
+	t.Time, _ = v.Time, v.Valid
+
+	return nil
+}
+
+// Value implements the [driver.Valuer] interface.
+func (t Time) Value() (driver.Value, error) {
+	return t.Time, nil
+}
+
+// /////////////////////////////////////////////////////////////////////////////
+
+type NullTime struct {
+	time.Time
+	Valid bool
+}
+
+func (t NullTime) MarshalJSON() ([]byte, error) {
 	if !t.Valid {
 		return []byte("null"), nil
 	}
@@ -28,7 +88,7 @@ func (t Time) MarshalJSON() ([]byte, error) {
 	return []byte(`"` + t.Time.Format(time.RFC3339) + `"`), nil
 }
 
-func (t *Time) UnmarshalJSON(data []byte) error {
+func (t *NullTime) UnmarshalJSON(data []byte) error {
 	s := strings.Trim(string(data), `"`)
 
 	if s == "null" || s == "" {
@@ -50,7 +110,7 @@ func (t *Time) UnmarshalJSON(data []byte) error {
 	return fmt.Errorf("invalid time format: %s", s)
 }
 
-func (t *Time) Parse(s string) error {
+func (t *NullTime) Parse(s string) error {
 	for _, format := range timeFormats {
 		tt, err := time.Parse(format, s)
 		if err == nil {
@@ -65,12 +125,12 @@ func (t *Time) Parse(s string) error {
 }
 
 // String returns the time in RFC3339 format.
-func (t Time) String() string {
+func (t NullTime) String() string {
 	return t.Time.Format(time.RFC3339)
 }
 
 // Scan implements the [Scanner] interface.
-func (t *Time) Scan(value any) error {
+func (t *NullTime) Scan(value any) error {
 	v := sql.NullTime{}
 	if err := v.Scan(value); err != nil {
 		return err
@@ -82,7 +142,7 @@ func (t *Time) Scan(value any) error {
 }
 
 // Value implements the [driver.Valuer] interface.
-func (t Time) Value() (driver.Value, error) {
+func (t NullTime) Value() (driver.Value, error) {
 	if !t.Valid {
 		return nil, nil
 	}
--- time_test.go
+++ time_test.go
@@ -30,7 +30,7 @@ func TestTime_UnmarshalJSON(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			var tr Time
+			var tr NullTime
 			for _, arg := range tt.args {
 				if err := tr.UnmarshalJSON(arg); (err != nil) != tt.wantErr {
 					t.Errorf("Time.UnmarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
