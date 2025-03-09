ffc4b8e3d4e0e90380081f3dcb4a832abae92cff
Eray Ates <eray.ates@worldline.com>
2025-03-09 13:44:19 +0100

    fix: update null
    Signed-off-by: Eray Ates <eray.ates@worldline.com>

--- null.go
+++ null.go
@@ -14,12 +14,37 @@ func NewNull[T any](v T) Null[T] {
 	return Null[T]{Null: sql.Null[T]{V: v, Valid: true}}
 }
 
+func NewNullWithValid[T any](v T, valid bool) Null[T] {
+	return Null[T]{Null: sql.Null[T]{V: v, Valid: valid}}
+}
+
+func NewNullFromPtr[T any](v *T) Null[T] {
+	if v == nil {
+		var zero T
+
+		return Null[T]{Null: sql.Null[T]{V: zero, Valid: false}}
+	}
+
+	return Null[T]{Null: sql.Null[T]{V: *v, Valid: true}}
+}
+
 func (n Null[T]) Ptr() *T {
 	if !n.Valid {
 		return nil
 	}
 
 	return &n.V
+}
+
+// ValueOrZero returns the inner value if valid, otherwise zero.
+func (n Null[T]) ValueOrZero() T {
+	if !n.Valid {
+		var zero T
+
+		return zero
+	}
+
+	return n.V
 }
 
 func (n Null[T]) MarshalJSON() ([]byte, error) {
